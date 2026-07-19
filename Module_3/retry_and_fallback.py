21169149ba9533509ec4ac057e0f31f00342f91a
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2026-07-19 22:40:54 +0530

    Added Topic 7 - Retry & Fallback in Module 3

---
+++ Module_3/retry_and_fallback.py
@@ -0,0 +1,30 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+# Fallback
+
+primary_llm = ChatOllama(model="llama3.2")
+backup_llm = ChatOllama(model = "qwen:4b")
+llm = primary_llm.with_fallbacks([backup_llm])
+
+# retry
+# llm = ChatOllama(model="llama3.2").with_retry()
+
+prompt = ChatPromptTemplate.from_template(
+"""
+Answer the following question:
+
+{question}
+"""
+)
+
+chain = (
+    prompt | llm | StrOutputParser()
+)
+
+answer = chain.invoke({
+    "question" : "What is Python?"
+})
+
+print(answer)
