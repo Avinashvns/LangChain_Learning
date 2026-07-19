50e19b21b5b7ba8523e3450e88f35e3d0897653b
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2026-07-19 22:07:50 +0530

    Added Topic 4 - Runnable Assign in Module 3

---
+++ Module_3/runnable_assign.py
@@ -0,0 +1,24 @@
+from langchain_core.runnables import RunnablePassthrough , RunnableLambda
+
+chain = RunnablePassthrough.assign(
+    length = RunnableLambda(
+        lambda x: len(x["text"])
+    )
+)
+
+result = chain.invoke({
+        "text": "LangChain"
+    })
+print(result)
+
+# Multiple Fields
+
+chains = RunnablePassthrough.assign(
+    upper = lambda x: x["text"].upper(),
+    lower = lambda x: x["text"].lower()
+)
+
+results = chains.invoke({
+    "text" : "Langchain"
+})
+print(results)
